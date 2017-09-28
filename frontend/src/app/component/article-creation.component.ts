e5794ceda129eb743883e9412f5c3629f1596cc6
valentine <v.shemyako@gmail.com>
2017-09-28 07:08:09 +0300

    Polish Article Creation Section UI

--- frontend/src/app/component/article-creation.component.ts
+++ frontend/src/app/component/article-creation.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 import {User} from "../model/user";
+import {News} from "../model/news";
 
+
+//TODO: Bind time
 @Component({
     selector: 'app-article-creation',
     templateUrl: '../../assets/html/article-creation.component.html',
@@ -10,7 +13,8 @@ import {User} from "../model/user";
 export class ArticleCreationComponent implements OnInit {
 
     private currentUser: User;
-    private personalFormControl: FormGroup;
+    private article: News;
+    private articleFormControl: FormGroup;
 
     /**
      * Error message which will be displayed in case of erroneous login/password input
@@ -22,10 +26,26 @@ export class ArticleCreationComponent implements OnInit {
      */
     private submitted: boolean = false;
 
+    /**
+     * Defines possible user roles
+     */
+    private categories = {
+        isGlobal: false,
+        isSport: false,
+        isArt: false,
+        isPolitics: false,
+        isJoy: false
+    };
+
     constructor() {
     }
 
     ngOnInit() {
+        this.article = new News();
+        this.articleFormControl = new FormGroup({
+            'validTitle':new FormControl((this.article.title, [Validators.required, Validators.minLength(5)])),
+            'validDescription': new FormControl(this.article.description, [Validators.required, Validators.minLength(20), Validators.maxLength(160)]),
+            'validArticle': new FormControl(this.article.article, [Validators.required, Validators.minLength(100), Validators.maxLength(1500)]),
+        });
     }
-
 }
